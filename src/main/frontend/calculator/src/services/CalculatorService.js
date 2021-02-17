ab19044d2dbb543f4445809b0c8f61af49c67c6e
Shivraj <shivrajganacharya1@gmail.com>
2021-02-17 17:42:21 +0530

    changing Dockerfile content

--- src/main/frontend/calculator/src/services/CalculatorService.js
+++ src/main/frontend/calculator/src/services/CalculatorService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/"
+const EMPLOYEE_API_BASE_URL = "http://localhost:8085/api/v1/"
 
 class EmployeeService {
     getSqroot(num) {
